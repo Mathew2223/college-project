8c12032e4b30c7d8371646dc240e9f46630a9665
Ситниковы <sitnikovmatvey2008@gmail.com>
2025-03-13 23:09:33 +0300

    add: new features for block winners

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ membersSwitch.addEventListener('click', () => {
     document.getElementById('first-year').classList.remove('active');
     document.getElementById('second-year').classList.remove('active');
     document.getElementById('third-year').classList.remove('active');
+    document.getElementById('fourth-year').classList.remove('active');
 
     document.getElementById('nineteen-art').style.display = 'none';
     document.getElementById('twenty-art').style.display = 'none';
     document.getElementById('first-art').style.display = 'none';
     document.getElementById('second-art').style.display = 'none';
     document.getElementById('third-art').style.display = 'none';
+    document.getElementById('fourth-art').style.display = 'none';
 
     if (circleLeft.style.display === 'none') {
         circleLeft.style.display = 'block';
@@ -47,6 +49,7 @@ const twenty_art = document.getElementById('twenty-art');
 const first_art = document.getElementById('first-art');
 const second_art = document.getElementById('second-art');
 const third_art = document.getElementById('third-art');
+const fourth_art = document.getElementById('fourth-art');
 
 // 2019 year
 document.getElementById('nineteen-year').addEventListener('click', function() {
@@ -55,6 +58,7 @@ document.getElementById('nineteen-year').addEventListener('click', function() {
     document.getElementById('first-year').classList.remove('active');
     document.getElementById('second-year').classList.remove('active');
     document.getElementById('third-year').classList.remove('active');
+    document.getElementById('fourth-year').classList.remove('active');
 
     this.classList.add('active');
 
@@ -62,18 +66,19 @@ document.getElementById('nineteen-year').addEventListener('click', function() {
         nineteen_art.style.display = 'flex';
     }
     else {
-
         nineteen_art.style.display = 'flex';
         twenty_art.style.display = 'none';
         first_art.style.display = 'none';
         second_art.style.display = 'none';
         third_art.style.display = 'none';
+        fourth_art.style.display = 'none';
     }
     document.getElementById('nineteen-year').style.cursor = 'default';
     document.getElementById('twenty-year').style.cursor = 'pointer';
     document.getElementById('first-year').style.cursor = 'pointer';
     document.getElementById('second-year').style.cursor = 'pointer';
     document.getElementById('third-year').style.cursor = 'pointer';
+    document.getElementById('fourth-year').style.cursor = 'pointer';
 });
 // 2020 year
 document.getElementById('twenty-year').addEventListener('click', function() {
@@ -82,6 +87,7 @@ document.getElementById('twenty-year').addEventListener('click', function() {
     document.getElementById('first-year').classList.remove('active');
     document.getElementById('second-year').classList.remove('active');
     document.getElementById('third-year').classList.remove('active');
+    document.getElementById('fourth-year').classList.remove('active');
 
     this.classList.add('active');
 
@@ -94,12 +100,14 @@ document.getElementById('twenty-year').addEventListener('click', function() {
         first_art.style.display = 'none';
         second_art.style.display = 'none';
         third_art.style.display = 'none';
+        fourth_art.style.display = 'none';
     }
     document.getElementById('twenty-year').style.cursor = 'default';
     document.getElementById('nineteen-year').style.cursor = 'pointer';
     document.getElementById('first-year').style.cursor = 'pointer';
     document.getElementById('second-year').style.cursor = 'pointer';
     document.getElementById('third-year').style.cursor = 'pointer';
+    document.getElementById('fourth-year').style.cursor = 'pointer';
 });
 // 2021 year
 document.getElementById('first-year').addEventListener('click', function() {
@@ -108,6 +116,7 @@ document.getElementById('first-year').addEventListener('click', function() {
     document.getElementById('first-year').classList.remove('active');
     document.getElementById('second-year').classList.remove('active');
     document.getElementById('third-year').classList.remove('active');
+    document.getElementById('fourth-year').classList.remove('active');
 
     this.classList.add('active');
 
@@ -120,12 +129,14 @@ document.getElementById('first-year').addEventListener('click', function() {
         first_art.style.display = 'flex';
         second_art.style.display = 'none';
         third_art.style.display = 'none';
+        fourth_art.style.display = 'none';
     }
     document.getElementById('first-year').style.cursor = 'default';
     document.getElementById('nineteen-year').style.cursor = 'pointer';
     document.getElementById('twenty-year').style.cursor = 'pointer';
     document.getElementById('second-year').style.cursor = 'pointer';
     document.getElementById('third-year').style.cursor = 'pointer';
+    document.getElementById('fourth-year').style.cursor = 'pointer';
 });
 // 2022 year
 document.getElementById('second-year').addEventListener('click', function() {
@@ -134,6 +145,7 @@ document.getElementById('second-year').addEventListener('click', function() {
     document.getElementById('first-year').classList.remove('active');
     document.getElementById('second-year').classList.remove('active');
     document.getElementById('third-year').classList.remove('active');
+    document.getElementById('fourth-year').classList.remove('active');
 
     this.classList.add('active');
 
@@ -146,12 +158,14 @@ document.getElementById('second-year').addEventListener('click', function() {
         first_art.style.display = 'none';
         second_art.style.display = 'flex';
         third_art.style.display = 'none';
+        fourth_art.style.display = 'none';
     }
     document.getElementById('second-year').style.cursor = 'default';
     document.getElementById('nineteen-year').style.cursor = 'pointer';
     document.getElementById('twenty-year').style.cursor = 'pointer';
     document.getElementById('first-year').style.cursor = 'pointer';
     document.getElementById('third-year').style.cursor = 'pointer';
+    document.getElementById('fourth-year').style.cursor = 'pointer';
 });
 // 2023 year
 document.getElementById('third-year').addEventListener('click', function() {
@@ -160,6 +174,7 @@ document.getElementById('third-year').addEventListener('click', function() {
     document.getElementById('first-year').classList.remove('active');
     document.getElementById('second-year').classList.remove('active');
     document.getElementById('third-year').classList.remove('active');
+    document.getElementById('fourth-year').classList.remove('active');
 
     this.classList.add('active');
 
@@ -172,13 +187,45 @@ document.getElementById('third-year').addEventListener('click', function() {
         first_art.style.display = 'none';
         second_art.style.display = 'none';
         third_art.style.display = 'flex';
+        fourth_art.style.display = 'none';
     }
     document.getElementById('third-year').style.cursor = 'default';
     document.getElementById('nineteen-year').style.cursor = 'pointer';
     document.getElementById('twenty-year').style.cursor = 'pointer';
     document.getElementById('first-year').style.cursor = 'pointer';
     document.getElementById('second-year').style.cursor = 'pointer';
+    document.getElementById('fourth-year').style.cursor = 'pointer';
 });
+//2024 year
+document.getElementById('fourth-year').addEventListener('click', function() {
+    document.getElementById('nineteen-year').classList.remove('active');
+    document.getElementById('twenty-year').classList.remove('active');
+    document.getElementById('first-year').classList.remove('active');
+    document.getElementById('second-year').classList.remove('active');
+    document.getElementById('third-year').classList.remove('active');
+    document.getElementById('fourth-year').classList.remove('active');
+
+    this.classList.add('active');
+
+    if (fourth_art.style.display === 'flex') {
+        fourth_art.style.display = 'flex';
+    }
+    else {
+        nineteen_art.style.display = 'none';
+        twenty_art.style.display = 'none';
+        first_art.style.display = 'none';
+        second_art.style.display = 'none';
+        third_art.style.display = 'none';
+        fourth_art.style.display = 'flex';
+    }
+    document.getElementById('third-year').style.cursor = 'pointer';
+    document.getElementById('nineteen-year').style.cursor = 'pointer';
+    document.getElementById('twenty-year').style.cursor = 'pointer';
+    document.getElementById('first-year').style.cursor = 'pointer';
+    document.getElementById('second-year').style.cursor = 'pointer';
+    document.getElementById('fourth-year').style.cursor = 'default';
+});
+
 
 function changeTextBasedOnScreenWidth() {
     const name_of_place_competition = document.querySelectorAll('.name_of_place-competition');
