a3ab4e04ca1cc2e9225227f2c821914e72b7d8bb
Ситниковы <sitnikovmatvey2008@gmail.com>
2025-03-28 23:08:44 +0300

    fix any bags for first section page

--- main.js
+++ main.js
@@ -197,18 +197,8 @@ document.getElementById('fourth-year').addEventListener('click', function() {
     document.getElementById('fourth-year').style.cursor = 'default';
 });
 
-const it_system_button = document.getElementById('it-system_button');
-it_system_button.addEventListener('click', function(){
-    const nav_winners_years = document.querySelector('.winners-years');
-    if (nav_winners_years.style.display === 'none') {
-        nav_winners_years.style.display = 'flex';
-    }
-    plus_teacher_button.classList.remove('active');
-    plus_teacher_button.style.cursor = 'pointer';
-    this.classList.add('active');
-    this.style.cursor = 'default';
-});
 
+// constants for dynamic blocks
 // plus_teacher_button
 // create child for for parent plus teacher block
 const nav_winner_plus_teacher_years = document.createElement('nav');
@@ -217,10 +207,10 @@ nav_winner_plus_teacher_years.className = 'winners-years';
 
 const plus_teacher_2019 = document.createElement('button');
 const plus_teacher_p_2019 = document.createElement('p');
-plus_teacher_p_2019.textContent = '2019';
-plus_teacher_p_2019.className = 'p-years';
 plus_teacher_2019.className = 'years-twentys';
 plus_teacher_2019.id = 'nineteen-year_plus_teacher_2019';
+plus_teacher_p_2019.textContent = '2019';
+plus_teacher_p_2019.className = 'p-years';
 plus_teacher_2019.appendChild(plus_teacher_p_2019);
 
 const plus_teacher_2020 = document.createElement('button');
@@ -235,6 +225,30 @@ let years_plus_teacher = [plus_teacher_2019, plus_teacher_2020];
 nav_winner_plus_teacher_years.append(...years_plus_teacher);
 
 const plus_teacher_button = document.getElementById('plus_teacher_button');
+
+
+
+// it system nav
+const it_system_button = document.getElementById('it-system_button');
+it_system_button.addEventListener('click', function(){
+    const nav_winners_years = document.querySelector('.winners-years');
+    if (nav_winners_years.style.display === 'none') {
+        nav_winners_years.style.display = 'flex';
+    }
+    plus_teacher_button.classList.remove('active');
+    plus_teacher_button.style.cursor = 'pointer';
+    this.classList.add('active');
+    this.style.cursor = 'default';
+
+    document.getElementById('nineteen-year_plus_teacher_2019').style.display = 'none';
+    document.getElementById('nineteen-year_plus_teacher_2020').style.display = 'none';
+
+    this.style.display = 'flex';
+    this.style.justifyContent = 'center';
+    this.style.alignItems = 'center';
+});
+
+// it plus teacher nav
 plus_teacher_button.addEventListener('click', function() {
     const nav_winners_years = document.querySelector('.winners-years');
     if (nav_winners_years.style.display === 'none') {
@@ -243,12 +257,11 @@ plus_teacher_button.addEventListener('click', function() {
     it_system_button.classList.remove('active');
     it_system_button.style.cursor = 'pointer';
 
-    document.getElementById('winners-years2').style.display = 'none';
-    // document.getElementById('nav-winner-plus-teacher-years').style.display = 'flex';
-    document.getElementById('results').appendChild(nav_winner_plus_teacher_years);
-
     this.classList.add('active');
-    this.style.cursor = 'default';
+    this.classList.contains('active') ? this.style.cursor = 'default' : this.style.cursor = 'pointer';
+
+    document.getElementById('winners-years2').style.display = 'none';
+    document.getElementById('results').appendChild(nav_winner_plus_teacher_years);
 
     nineteen_art.style.display = 'none';
     twenty_art.style.display = 'none';
@@ -256,6 +269,28 @@ plus_teacher_button.addEventListener('click', function() {
     second_art.style.display = 'none';
     third_art.style.display = 'none';
     fourth_art.style.display = 'none';
-    // const nineteen_year_plus_teacher_2019 = document.getElementById('nineteen-year_plus_teacher_2019');
-    // nineteen_year_plus_teacher_2019.style.display = 'flex';
+
+    document.getElementById('nineteen-year_plus_teacher_2019').style.display = 'flex';
+    document.getElementById('nineteen-year_plus_teacher_2019').style.justifyContent = 'center';
+    document.getElementById('nineteen-year_plus_teacher_2020').style.display = 'flex';
+    document.getElementById('nineteen-year_plus_teacher_2020').style.justifyContent = 'center';
+
+    nav_winner_plus_teacher_years.style.display = 'flex';
+});
+plus_teacher_2019.addEventListener('click', function() {
+    document.getElementById('nineteen-year_plus_teacher_2019').classList.remove('active');
+    document.getElementById('nineteen-year_plus_teacher_2020').classList.remove('active');
+
+    this.classList.add('active');
+    this.style.cursor = 'default';
+    plus_teacher_2020.style.cursor = 'pointer';
+});
+plus_teacher_2020.addEventListener('click', function() {
+    document.getElementById('nineteen-year_plus_teacher_2019').classList.remove('active');
+    document.getElementById('nineteen-year_plus_teacher_2020').classList.remove('active');
+
+    this.classList.add('active');
+
+    plus_teacher_2019.style.cursor = 'pointer';
+    this.style.cursor = 'default';
 });
--- script.js
+++ script.js
@@ -22,6 +22,8 @@ membersSwitch.addEventListener('click', () => {
     if (circleLeft.style.display === 'none') {
         circleLeft.style.display = 'block';
         circleRight.style.display = 'none';
+        document.getElementById('nav-winner-plus-teacher-years').removeAttribute('style');
+        document.getElementById('it-system_button').removeAttribute('style');
     } else {
         circleLeft.style.display = 'none';
         circleRight.style.display = 'block';
@@ -33,6 +35,43 @@ function changeTextBasedOnScreenWidth() {
     name_of_place_competition.forEach((element) => {
         if (window.innerWidth < 1768) {
             element.textContent = 'КОГПОБУ СКПиСО';
+        }
+    });
+
+    const special_window_size = document.querySelectorAll('.progr-speciality_p');
+    special_window_size.forEach((item) => {
+        if (window.innerWidth >= 2100) {
+            item.textContent = 'Программирование в компьютерных системах';
+        }
+        else if (window.innerWidth < 2100 && window.innerWidth > 1150) {
+            item.textContent = '09.02.03 Программирование в компьютерных системах';
+        }
+        else if (window.innerWidth <= 1150) {
+            item.innerHTML = '<abbr style="text-decoration: underline;" title="Программирование в компьютерных системах">09.02.03</abbr>';
+        }
+        // Try:
+        // item.addEventListener('mouseenter', () => {
+        //     const tooltip = document.createElement('span');
+        //     tooltip.className = 'tooltip';
+        //     tooltip.textContent = 'Программирование в компьютерных системах';
+        //     document.body.appendChild(tooltip);
+     
+        //     const rect = item.getBoundingClientRect();
+        //     tooltip.style.position = 'absolute';
+        //     tooltip.style.left = `${rect.left}px`;
+        //     tooltip.style.top = `${rect.bottom}px`;
+        // });
+        // item.addEventListener('mouseleave', () => {
+        //     const tooltip = document.querySelector('.tooltip');
+        //     if (tooltip) {
+        //         tooltip.remove();
+        //     }
+        // });
+    });
+
+    document.querySelectorAll('.members-switch-text').forEach((item) => {
+        if (window.innerWidth < 500) {
+            item.textContent = 'Результаты';
         }
     });
 }
